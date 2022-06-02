9bdd8d9f1fa83219567cfbcbf747000e91598014
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-02 11:20:32 -0400

    Create Apollo GraphQL server

--- source/main.ts
+++ source/main.ts
@@ -1,17 +1,12 @@
 import "reflect-metadata";
 import { AppDataSource } from "./DataSource";
-import { User } from "./entity/User";
+import { createApolloServer } from './server';
 
-AppDataSource.initialize().then(async (dataSource) => {
-  console.log("Inserting a new user into the database...");
-  const user = new User();
-  user.firstName = "Timber";
-  user.lastName = "Saw";
-  user.age = 25;
-  await dataSource.manager.save(user);
-  console.log("Saved a new user with id: " + user.id);
+async function main() {
+  await AppDataSource.initialize()
+  const { url } = await createApolloServer()
 
-  console.log("Loading users from the database...");
-  const users = await dataSource.manager.find(User);
-  console.log("Loaded users: ", users);
-})
+  console.log(`🚀 Server ready at ${url}`);
+}
+
+main()
---
+++ source/server.ts
@@ -0,0 +1,35 @@
+
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import http from 'http';
+import "reflect-metadata";
+import { buildSchema } from 'type-graphql';
+
+async function createApolloServer({ port = 4000 }: { port?: number } = {}) {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+
+  const schema = await buildSchema({
+    resolvers: [__dirname + "/resolver/*.resolver.ts"],
+  })
+
+  const server = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  await new Promise<void>(resolve => httpServer.listen(port, resolve));
+
+  return {
+    server,
+    url: `http://localhost:${port}${server.graphqlPath}`
+  }
+}
+
+export { createApolloServer }
