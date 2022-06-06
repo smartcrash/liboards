c887e56992f0e44aa9eb416112ebf847b11ceb98
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-06 08:25:17 -0400

    Update Session type

--- source/types.ts
+++ source/types.ts
@@ -2,7 +2,7 @@ import { type Request, type Response } from "express"
 import { type DataSource } from "typeorm"
 
 export type TContext = {
-  req: Request & { session: Request['session'] & { userId: number } }
+  req: Request & { session: Request['session'] & { userId?: number } }
   res: Response
   dataSource: DataSource
 }
