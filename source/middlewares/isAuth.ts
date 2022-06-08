01c8a5216d01b6a3a1e75576ab50ae230d7dc7b8
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-08 09:09:56 -0400

    Create isAuth middleware

---
+++ source/middlewares/isAuth.ts
@@ -0,0 +1,11 @@
+import { MiddlewareFn } from "type-graphql";
+import { TContext } from '../types'
+
+
+export const isAuth: MiddlewareFn<TContext> = async ({ context }, next) => {
+  if (!context.req.session.userId) {
+    throw new Error("not authenticated");
+  }
+
+  next()
+}
