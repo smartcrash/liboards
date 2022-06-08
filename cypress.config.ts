c79e3a30f59eca732cd0c14c8262f33e5d0e23f2
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-08 09:10:07 -0400

    Move cypress to root folder

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) { },
-    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
