de36123741c4c9997be3b2306873b769440f4b33
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-10 11:39:44 -0400

    Rename file

--- client/source/main.tsx
+++ client/source/main.tsx
@@ -1,20 +1,17 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider as UrqlProvider } from "urql";
 import App from "./App";
 import theme from "./theme";
-import { createUrqlClient } from "./urql";
+import { createUrqlClient } from "./createUrqlClient";
 
 const client = createUrqlClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <UrqlProvider value={client}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
-    </UrqlProvider>
-  </StrictMode>
+  <UrqlProvider value={client}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </UrqlProvider>
 );
