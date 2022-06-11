0a549f7215e5f002bda21f83b0ab6c46bc70e1f3
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-11 14:05:56 -0400

    Update project-view.cy.ts

--- cypress/e2e/project-view.cy.ts
+++ cypress/e2e/project-view.cy.ts
@@ -1,27 +1,16 @@
 import Chance from "chance"
 
 const chance = new Chance()
-let url = ''
 
 describe('Project View', () => {
-  before(() => {
-    // cy.exec('npm run schema:drop')
-    // cy.exec('npm run schema:sync')
+  before(() => cy.createUser())
 
-    cy.createUser()
-
-    // Create project to run the test agains
-    cy.getByTestId('new-project').click()
-    cy.location("pathname").should("equal", "/projects/new");
-
+  beforeEach(() => {
+    cy.visit("/projects/new")
     cy.getByTestId('title').clear().type(chance.sentence({ words: 2 }))
     cy.getByTestId('description').clear().type(chance.sentence({ words: 5 }))
     cy.getByTestId('submit').click()
-
-    cy.location('pathname').then((pathname) => url = pathname)
   })
-
-  beforeEach(() => cy.visit(url))
 
   it('should allow to edit the project\'s `title` and `description`', () => {
     const title = chance.sentence({ words: 2 })
