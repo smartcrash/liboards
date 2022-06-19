6ed42a8e3ed84066dab1eeee645cfa125a96df43
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-19 17:19:47 -0400

    Move dataSource options

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,23 +1,4 @@
-
-import { DataSourceOptions } from "typeorm";
-import { DB_CONNECTION, DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME } from "./source/constants";
-
-const config = {
-  type: DB_CONNECTION,
-  database: DB_DATABASE,
-  host: DB_HOST,
-  port: DB_PORT,
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  logging: false,
-  synchronize: true,
-  dropSchema: false,
-  migrationsRun: true,
-  entities: ["source/entity/*.ts"],
-  migrations: ["source/migrations/*.ts"],
-  subscribers: ["source/subscribers/*.ts"],
-  seeds: ['source/seeds/*.ts'],
-  factories: ['source/factories/*.ts'],
-} as DataSourceOptions
+// NOTE: This file is for `typeorm-seeding` connection config
+import { config } from './source/dataSource'
 
 export default config
--- source/dataSource.ts
+++ source/dataSource.ts
@@ -1,5 +1,25 @@
 
-import { DataSource } from "typeorm";
-import config from "../ormconfig"
+import { DataSource, DataSourceOptions } from "typeorm";
+import { DB_CONNECTION, DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME } from "./constants";
+
+export const config = {
+  type: DB_CONNECTION,
+  database: DB_DATABASE,
+  host: DB_HOST,
+  port: DB_PORT,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  logging: false,
+  synchronize: true,
+  dropSchema: false,
+  migrationsRun: true,
+  entities: ["source/entity/*.ts"],
+  migrations: ["source/migrations/*.ts"],
+  subscribers: ["source/subscribers/*.ts"],
+
+  // See: https://www.npmjs.com/package/typeorm-seeding#%E2%9D%AF-installation
+  seeds: ['source/seeds/*.ts'],
+  factories: ['source/factories/*.ts'],
+} as DataSourceOptions
 
 export const dataSource = new DataSource(config)
