b3334e1e3a32eb935c5d7434ac7ab36b4352d80c
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-24 15:25:48 -0400

    Remove pages/index.ts

--- client/source/pages/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './loading'
-
-export * from './login'
-export * from './sign-up'
-export * from './reset-password'
-export * from './forgot-password'
-
-export * from './dashboard'
-export * from './projects'
