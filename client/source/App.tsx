23ca514fb356edccaa85748490d1ca4440a2661a
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-24 15:15:31 -0400

    Use lazy loading on router

--- client/source/App.tsx
+++ client/source/App.tsx
@@ -1,17 +1,18 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import { useCurrentUserQuery } from "./generated/graphql";
-import {
-  CreateProject,
-  Dashboard,
-  ForgotPassword,
-  ListProjects,
-  Loading,
-  Login,
-  ResetPassword,
-  ShowProject,
-  SignUp,
-} from "./pages";
+import Loading from "./pages/loading";
 import { routes } from "./routes";
+
+const ForgotPassword = lazy(() => import("./pages/forgot-password"));
+const Login = lazy(() => import("./pages/login"));
+const ResetPassword = lazy(() => import("./pages/reset-password"));
+const SignUp = lazy(() => import("./pages/sign-up"));
+
+const Dashboard = lazy(() => import("./pages/dashboard"));
+const ListProjects = lazy(() => import("./pages/projects/list"));
+const CreateProject = lazy(() => import("./pages/projects/create"));
+const ShowProject = lazy(() => import("./pages/projects/show"));
 
 function App() {
   const [{ data, fetching }] = useCurrentUserQuery();
@@ -19,29 +20,31 @@ function App() {
 
   return (
     <Router>
-      <Routes>
-        {fetching ? (
-          <Route path="*" element={<Loading />} />
-        ) : user ? (
-          <>
-            <Route path={routes.index} element={<Dashboard />}>
-              <Route path={routes.index} element={<Navigate to={routes["projects.list"]} replace />} />
-              <Route path={routes["projects.list"]} element={<ListProjects />} />
-              <Route path={routes["projects.create"]} element={<CreateProject />} />
-              <Route path={routes["projects.show"]} element={<ShowProject />} />
-            </Route>
-            <Route path="*" element={<Navigate to={routes.index} replace />} />
-          </>
-        ) : (
-          <>
-            <Route path={routes.login} element={<Login />} />
-            <Route path={routes.signUp} element={<SignUp />} />
-            <Route path={routes.forgotPwd} element={<ForgotPassword />} />
-            <Route path={routes.resetPwd} element={<ResetPassword />} />
-            <Route path="*" element={<Navigate to={routes.login} replace />} />
-          </>
-        )}
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          {fetching ? (
+            <Route path="*" element={<Loading />} />
+          ) : user ? (
+            <>
+              <Route path={routes.index} element={<Dashboard />}>
+                <Route path={routes.index} element={<Navigate to={routes["projects.list"]} replace />} />
+                <Route path={routes["projects.list"]} element={<ListProjects />} />
+                <Route path={routes["projects.create"]} element={<CreateProject />} />
+                <Route path={routes["projects.show"]} element={<ShowProject />} />
+              </Route>
+              <Route path="*" element={<Navigate to={routes.index} replace />} />
+            </>
+          ) : (
+            <>
+              <Route path={routes.login} element={<Login />} />
+              <Route path={routes.signUp} element={<SignUp />} />
+              <Route path={routes.forgotPwd} element={<ForgotPassword />} />
+              <Route path={routes.resetPwd} element={<ResetPassword />} />
+              <Route path="*" element={<Navigate to={routes.login} replace />} />
+            </>
+          )}
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
