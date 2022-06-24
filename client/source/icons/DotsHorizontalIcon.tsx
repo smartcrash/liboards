2ce9827c06139610832f9c294185ab218ef3336f
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-24 09:35:03 -0400

    Fix Icons attr names

--- client/source/icons/DotsHorizontalIcon.tsx
+++ client/source/icons/DotsHorizontalIcon.tsx
@@ -11,9 +11,9 @@ export const DotsHorizontalIcon = createIcon({
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <circle cx="12" cy="12" r="1"></circle>
       <circle cx="19" cy="12" r="1"></circle>
