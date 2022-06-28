6601770b42603ac49e759369a78690fd38ebec77
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-28 13:32:20 -0400

    Update App.tsx
    Create `loadable` to wrap lazy loaded pages

--- client/source/App.tsx
+++ client/source/App.tsx
@@ -4,15 +4,25 @@ import { useCurrentUserQuery } from "./generated/graphql";
 import Loading from "./pages/loading";
 import { routes } from "./routes";
 
-const ForgotPassword = lazy(() => import("./pages/forgot-password"));
-const Login = lazy(() => import("./pages/login"));
-const ResetPassword = lazy(() => import("./pages/reset-password"));
-const SignUp = lazy(() => import("./pages/sign-up"));
+const loadable = (factory: Parameters<typeof lazy>[0]) => () => {
+  const Component = lazy(factory);
 
-const Dashboard = lazy(() => import("./pages/dashboard"));
-const ListProjects = lazy(() => import("./pages/projects/list"));
-const CreateProject = lazy(() => import("./pages/projects/create"));
-const ShowProject = lazy(() => import("./pages/projects/show"));
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <Component />
+    </Suspense>
+  );
+};
+
+const ForgotPassword = loadable(() => import("./pages/forgot-password"));
+const Login = loadable(() => import("./pages/login"));
+const ResetPassword = loadable(() => import("./pages/reset-password"));
+const SignUp = loadable(() => import("./pages/sign-up"));
+
+const Dashboard = loadable(() => import("./pages/dashboard"));
+const ListProjects = loadable(() => import("./pages/projects/list"));
+const CreateProject = loadable(() => import("./pages/projects/create"));
+const ShowProject = loadable(() => import("./pages/projects/show"));
 
 function App() {
   const [{ data, fetching }] = useCurrentUserQuery();
@@ -20,31 +30,29 @@ function App() {
 
   return (
     <Router>
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          {fetching ? (
-            <Route path="*" element={<Loading />} />
-          ) : user ? (
-            <>
-              <Route path={routes.index} element={<Dashboard />}>
-                <Route path={routes.index} element={<Navigate to={routes["projects.list"]} replace />} />
-                <Route path={routes["projects.list"]} element={<ListProjects />} />
-                <Route path={routes["projects.create"]} element={<CreateProject />} />
-                <Route path={routes["projects.show"]} element={<ShowProject />} />
-              </Route>
-              <Route path="*" element={<Navigate to={routes.index} replace />} />
-            </>
-          ) : (
-            <>
-              <Route path={routes.login} element={<Login />} />
-              <Route path={routes.signUp} element={<SignUp />} />
-              <Route path={routes.forgotPwd} element={<ForgotPassword />} />
-              <Route path={routes.resetPwd} element={<ResetPassword />} />
-              <Route path="*" element={<Navigate to={routes.login} replace />} />
-            </>
-          )}
-        </Routes>
-      </Suspense>
+      <Routes>
+        {fetching ? (
+          <Route path="*" element={<Loading />} />
+        ) : user ? (
+          <>
+            <Route path={routes.index} element={<Dashboard />}>
+              <Route path={routes.index} element={<Navigate to={routes["projects.list"]} replace />} />
+              <Route path={routes["projects.list"]} element={<ListProjects />} />
+              <Route path={routes["projects.create"]} element={<CreateProject />} />
+              <Route path={routes["projects.show"]} element={<ShowProject />} />
+            </Route>
+            <Route path="*" element={<Navigate to={routes.index} replace />} />
+          </>
+        ) : (
+          <>
+            <Route path={routes.login} element={<Login />} />
+            <Route path={routes.signUp} element={<SignUp />} />
+            <Route path={routes.forgotPwd} element={<ForgotPassword />} />
+            <Route path={routes.resetPwd} element={<ResetPassword />} />
+            <Route path="*" element={<Navigate to={routes.login} replace />} />
+          </>
+        )}
+      </Routes>
     </Router>
   );
 }
